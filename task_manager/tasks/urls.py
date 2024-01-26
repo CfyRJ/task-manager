5c539020224077283aad2967883da43a615d1bee
Alexandr Kositskiy <koso-96@mail.ru>
2024-01-27 00:58:30 +0300

    Application tasks has been completed

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexSTasks.as_view(), name='index_tasks'),
-    path('create/', views.CreateTask.as_view(), name='create_tasks'),
-    path('<int:pk>/update/', views.UpdateTask.as_view(), name='update_tasks'),
-    path('<int:pk>/delete/', views.DeleteTask.as_view(), name='delete_tasks'),
+    path('create/', views.CreateTask.as_view(), name='create_task'),
+    path('<int:pk>/update/', views.UpdateTask.as_view(), name='update_task'),
+    path('<int:pk>/delete/', views.DeleteTask.as_view(), name='delete_task'),
     path('<int:pk>/', views.ShowTask.as_view(), name='show_task'),
 ]
--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,22 +1,99 @@
+from typing import Any
+from django.forms.forms import BaseForm
+from django.http import HttpRequest, HttpResponse
+from django.http import request
+
+from django.forms.models import BaseModelForm
 from django.shortcuts import render
 from django.views import View
+from django.contrib import messages
+from .models import Tasks
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.contrib.messages.views import SuccessMessageMixin
+from .forms import TaskForm
+
+from django.urls import reverse_lazy
+from django.utils.translation import gettext as _
+
+from django.shortcuts import redirect
 
 
-class CreateTask(View):
-    pass
+class MixinMessage(LoginRequiredMixin):
+    redirect_field_name = ""
+
+    def get_login_url(self, *args, **kwargs):
+        messages.add_message(self.request, messages.ERROR,
+                             'You are not authorized! Please come in.')
+        return super().get_login_url()
 
 
-class IndexSTasks(View):
-    pass
+class IndexSTasks(MixinMessage,  View):
+    def get(self, request, *args, **kwargs):
+        tasks = Tasks.objects.order_by('id')
+
+        messages_ = messages.get_messages(request)
+        return render(request,
+                      'tasks/index_tasks.html',
+                      context={
+                          'messages': messages_,
+                          'tasks': tasks,
+                      })
 
 
-class UpdateTask(View):
-    pass
+class CreateTask(MixinMessage, SuccessMessageMixin, CreateView):
+    form_class = TaskForm
+    template_name = 'tasks/create_task.html'
+    extra_context = {'title': 'Create task'}
+    success_url = reverse_lazy('index_tasks')
+    success_message = 'Task successfully created'
+
+    def form_valid(self, form: BaseForm):
+        instance = form.save(commit=False)
+        instance.author = self.request.user
+        instance.save()
+
+        messages.add_message(self.request, messages.SUCCESS, self.success_message)
+
+        return redirect(self.success_url)
 
 
-class DeleteTask(View):
-    pass
+class UpdateTask(MixinMessage, SuccessMessageMixin, UpdateView):
+    model = Tasks
+    form_class = TaskForm
+    template_name = 'tasks/update_task.html'
+    extra_context = {'title': 'Change of task'}
+    success_url = reverse_lazy('index_tasks')
+    success_message = 'Task successfully changed'
+
+
+class DeleteTask(MixinMessage, SuccessMessageMixin, DeleteView):
+    model = Tasks
+    template_name = 'tasks/delete_task.html'
+    extra_context = {'title': 'Deleting a task'}
+    success_url = reverse_lazy('index_tasks')
+    success_message = 'Task deleted successfully'
+
+    def get(self, request: HttpRequest,
+            *args: str, **kwargs: Any) -> HttpResponse:
+        user = request.user
+        id_task = kwargs.get('pk')
+        author = Tasks.objects.get(id=id_task).author
+        if user != author:
+            messages.add_message(self.request, messages.ERROR,
+                                 'Only its author can delete a task')
+            return redirect(reverse_lazy('index_tasks'))
+
+        return super().get(request, *args, **kwargs)
 
 
 class ShowTask(View):
-    pass
+    def get(self, request, *args, **kwargs):
+        id_task = kwargs.get('pk')
+        task = Tasks.objects.get(id=id_task)
+        return render(request,
+                      'tasks/show_task.html',
+                      context={
+                          'task': task,
+                      })
